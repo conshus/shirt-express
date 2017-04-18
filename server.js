55e559d1dba37e212c8d19bd91900a749c63a006
Dwane <Dwane@hiphopandcode.com>
2017-04-18 19:58:54 -0400

    cleaned up some code

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.get('/tshirt/:shirtId', function (request, response) {
   let index = parseInt(shirtId);
   let shirt = catalog[index];
   response.send(shirt);
-  response.send(color);
 })
 app.listen(3000, function(){
 console.log('The express server is now listening at port 3000. To see it in action, open your broweser and enter localhost:3000/greeting in the address bar.');
